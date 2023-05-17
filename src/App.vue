b5db04a0ba64a044556a233065a945e7ffed2e1d
Max <126419161+maxb6299@users.noreply.github.com>
2023-05-17 19:25:39 -0400

    Set up basic structure for frontend

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GetUserData />
+  <ShowUserData />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GetUserData from './components/GetUserData.vue'
+import ShowUserData from './components/ShowUserData.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GetUserData,
+    ShowUserData
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
