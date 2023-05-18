fa7db9ad5d9d1f54ff9cdb8f435aa72038c42699
Max <126419161+maxb6299@users.noreply.github.com>
2023-05-18 19:05:09 -0400

    Copy over setAddress function

--- src/components/GetUserData.vue
+++ src/components/GetUserData.vue
@@ -12,12 +12,13 @@
 
 
     <!-- for testing purposes -->
-    <!-- <div>
+    <div>
         Name: {{ user.name }} <br>
         Phone Number: {{ user.phoneNumber }} <br>
         Latitude: {{ user.latitude }} <br>
         Longitude: {{ user.longitude }} <br>
-    </div> -->
+        Address: {{ user.address }} <br>
+    </div>
 </template>
 
 <script>
@@ -28,7 +29,8 @@
                     name: '',
                     phoneNumber: '',
                     latitude: '',
-                    longitude: ''
+                    longitude: '',
+                    address: ''
                 },
 
                 showLocationFailureMessage: false,
@@ -47,6 +49,8 @@
                         this.showLocationFailureMessage = false;
                         this.showGetLocationButton = false; false
                         this.showSubmitButton = true;
+
+                        this.setAddress(); 
                     }, failure => { // user denied location
                         this.showLocationFailureMessage = true;
                     });
@@ -54,6 +58,28 @@
                     console.log("Error. This browser does not support geolocation")
                     this.showLocationFailureMessage = true;
                 }
+            },
+
+            // using Google Maps API, it converts current latitude and longitude to an address and sets user.address to that value
+            setAddress () {
+                const lat = this.user.latitude;
+                const lon = this.user.longitude;
+                const API_KEY = 'AIzaSyBD0dirUoiHMG-LiLDqJ4-ZloDzV1DueKg';
+
+                const addressUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&location_type=ROOFTOP&result_type=street_address&key=${API_KEY}`;
+
+                fetch(addressUrl)
+                    .then(req => req.json()) // converts data in url to .json
+                    .then(addressData => {
+                        if (addressData.status == 'OK') {
+                            this.user.address = addressData.results[0].formatted_address;
+                        } else if (addressData.status == 'ZERO_RESULTS') { // if coordinates don't lead to an address
+                            this.user.address = `Address not found from (${lat}, ${lon})`; // shows coordinates of location with message
+                        } else {
+                            this.user.address = `API Error. Coordinates: (${lat}, ${lon})`; // shows cooridnates of location with error
+                        }
+                    })
+                    .catch(error => console.log("Error" + error))
             },
 
             sendData() {
