e33a6aa03b50686121e11a65af0f62a9a2ca51e2
Max <126419161+maxb6299@users.noreply.github.com>
2023-05-18 11:38:41 -0400

    Write button for asking user location Write variables for user location Write getLocation() function

--- src/components/GetUserData.vue
+++ src/components/GetUserData.vue
@@ -3,6 +3,10 @@
         Please input the following: <br>
         Name: <input required v-model="user.name" > <br>
         Phone Number: <input required v-model="user.phoneNumber" type="tel" pattern="[0-9]{10}"> <br>
+
+        <button @click="getLocation">Share your location</button>
+        <div v-if="showLocationFailureMessage" >Error. Please share your location</div>
+
         <input type="submit" value="Submit Data">
     </form>
 
@@ -11,6 +15,8 @@
     <!-- <div>
         Name: {{ user.name }} <br>
         Phone Number: {{ user.phoneNumber }} <br>
+        Latitude: {{ user.latitude }} <br>
+        Longitude: {{ user.longitude }} <br>
     </div> -->
 </template>
 
@@ -21,6 +27,27 @@
                 user: {
                     name: '',
                     phoneNumber: '',
+                    latitude: '',
+                    longitude: ''
+                },
+
+                showLocationFailureMessage: false
+            }
+        },
+
+        methods: {
+            getLocation() {
+                if (navigator.geolocation) {
+                    navigator.geolocation.getCurrentPosition(position => {
+                        this.user.latitude = position.coords.latitude;
+                        this.user.longitude = position.coords.longitude; 
+                        this.showLocationFailureMessage = false;
+                    }, failure => { // user denied location
+                        this.showLocationFailureMessage = true;
+                    });
+                } else {
+                    console.log("Error. This browser does not support geolocation")
+                    this.showLocationFailureMessage = true;
                 }
             }
         }
