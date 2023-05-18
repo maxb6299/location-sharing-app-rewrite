55d0e962508835dac75d90f6457bc9dd98c3d4b0
Max <126419161+maxb6299@users.noreply.github.com>
2023-05-18 11:18:22 -0400

    Write form for name and number Write data variables for name and number Write test div

--- src/components/GetUserData.vue
+++ src/components/GetUserData.vue
@@ -1,7 +1,28 @@
 <template>
+    <form>
+        Please input the following: <br>
+        Name: <input required v-model="user.name" > <br>
+        Phone Number: <input required v-model="user.phoneNumber" type="tel" pattern="[0-9]{10}"> <br>
+        <input type="submit" value="Submit Data">
+    </form>
 
+
+    <!-- for testing purposes -->
+    <!-- <div>
+        Name: {{ user.name }} <br>
+        Phone Number: {{ user.phoneNumber }} <br>
+    </div> -->
 </template>
 
-<scripting>
-
-</scripting>
+<script>
+    export default {
+        data() {
+            return {
+                user: {
+                    name: '',
+                    phoneNumber: '',
+                }
+            }
+        }
+    }
+</script>
